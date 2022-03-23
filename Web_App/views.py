8b64e854d4c88975ff5e9d7dd28b39aa7476154e
mahendrahd1995@gmail.com <mahendrahd1995@gmail.com>
2022-03-23 05:54:59 +0530

    django aws question answer radio button

--- Web_App/views.py
+++ Web_App/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
+from matplotlib.style import context
 
 
 # Create your views here.
@@ -21,8 +22,7 @@ def about_me(request):
     return render(request,'web_app/about_me.html')
 
 def aws_test(request):  
-    context = {}
-    context['form'] = ExampleForm()
+   
     
     context = {
         "data" : ['SQS','Cloud Trail','EC2','Lambda','IAM','SNS','Cloud Watch','VPC'],
@@ -39,6 +39,7 @@ def aws_test(request):
 #     return render(request,'web_app/aws_start_test.html')
 
 def aws_start_test(request):
+    
     try:
         if request.method=="POST":
             for q in request.POST.items():
@@ -46,7 +47,45 @@ def aws_start_test(request):
     except:
         pass
 
-    return render(request,'web_app/aws_start_test.html')
+    import psycopg2
+
+    try:
+            connection = psycopg2.connect(user="postgres",
+                                  password="password",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="aws_preparation")
+            cursor = connection.cursor()
+            postgreSQL_select_Query = "select * from EC2"
+
+            cursor.execute(postgreSQL_select_Query)
+            print("Selecting rows from mobile table using cursor.fetchall")
+            mobile_records = cursor.fetchall()
+            
+
+            print("Print each row and it's columns values")
+            for row in mobile_records:
+                print("Id = ", row[0], )
+                print("Question = ", row[1])
+                print("A  = ", row[2])
+                print("B  = ", row[3])
+                print("C  = ", row[4])
+                print("D  = ", row[5])
+                print("E  = ", row[6])
+                print("Answer  = ", row[7])
+
+    except (Exception, psycopg2.Error) as error:
+            print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+         # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    print("outside loop : \n")
+    print(mobile_records)
+    return render(request,'web_app/aws_start_test.html',{'aws_questions':mobile_records})
 
 @login_required(login_url='login')
 def home(request):
